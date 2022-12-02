219a3d51068dbf8eaf2718f4716fa62787b70a5c
Mayalen Etcheverry <etcheverry.mayalen@gmail.com>
2022-12-02 11:07:56 -0500

    delete empty tests

--- test/test_selector.py
+++
@@ -1,8 +0,0 @@
-from autodiscjax.modules.sgdoptimizer import SGDOptimizer
-import jax.numpy as jnp
-import jax.random as jrandom
-
-def test_nearest_neighbors():
-    key = jrandom.PRNGKey(0)
-
-    sgdoptimizer = SGDOptimizer()
