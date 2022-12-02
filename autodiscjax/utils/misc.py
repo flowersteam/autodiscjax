368f0ff5c563e54cd632e0a7a0912c2b58ebaa64
Mayalen Etcheverry <etcheverry.mayalen@gmail.com>
2022-12-02 11:14:43 -0500

    implementation of walls

--- autodiscjax/utils/misc.py
+++ autodiscjax/utils/misc.py
@@ -64,3 +64,161 @@ def softplus(x):
 @jit
 def inv_softplus(x):
     return jnp.log(jnp.exp(x/100.0) - 1.0)
+
+@jit
+def flat_top_gaussian(x, x0, sigma, A, P):
+    return A*jnp.exp(-((x-x0)**2/(2*sigma**2))**P)
+
+@jit
+def sigmoid(x):
+    return 1 / (1 + jnp.exp(-x))
+
+@jit
+def calc_segment_intersection(seg1_start, seg1_end, seg2_start, seg2_end):
+    seg1 = seg1_end - seg1_start
+    seg1_perp = jnp.array([-seg1[1], seg1[0]])
+    seg2 = seg2_end - seg2_start
+
+    v1 = seg1_start - seg2_start
+    t1 = jnp.cross(seg2, v1) / jnp.dot(seg2, seg1_perp)
+    t2 = jnp.dot(v1, seg1_perp) / jnp.dot(seg2, seg1_perp)
+
+    p = seg1_start + t1 * seg1
+
+    return p, t1, t2
+
+@jit
+def wall_sticky_collision(traj_start, traj_end, wall_start, wall_end):
+    """
+    Returns the new trajectory endpoint when the trajectory is colliding with a "sticky" wall.
+
+    Parameters
+    ---------
+    traj_start, traj_end, wall_start, wall_end: points (2D or 3D array)
+
+    Returns
+    ---------
+    t1:
+        intersection "time" (0<=t<=1) along traj segment if there is an intersection
+        NaN if traj and wall segments are colinear or have no intersection
+    p:
+        new traj_end point after collision
+        traj_end if traj and wall segments are colinear or have no intersection
+    """
+    p, t1, t2 = calc_segment_intersection(traj_start, traj_end, wall_start, wall_end)
+    return lax.cond((t1 >= 0.0) & (t1 <= 1.0) & (t2 >= 0.0) & (t2 <= 1.0), lambda: (t1, p), lambda: (float("nan")*jnp.ones_like(t1), traj_end))
+
+
+@jit
+def wall_elastic_collision(traj_start, traj_end, wall_start, wall_end):
+    """
+    Returns the new trajectory endpoint if the trajectory is colliding with a wall (assuming elastic collision).
+
+    Parameters
+    ---------
+    traj_start, traj_end, wall_start, wall_end: points (2D or 3D array)
+
+    Returns
+    ---------
+    t1:
+        intersection "time" (0<=t<=1) along traj segment if there is an intersection
+        NaN if traj and wall segments are colinear or have no intersection
+    p:
+        new traj_end point after collision
+        traj_end if traj and wall segments are colinear or have no intersection
+    """
+    p, t1, t2 = calc_segment_intersection(traj_start, traj_end, wall_start, wall_end)
+
+    traj_parallel = traj_end-traj_start
+    wall_parallel = wall_end - wall_start
+    wall_perp = jnp.array([-wall_parallel[1], wall_parallel[0]])
+
+    v_parallel = jnp.dot(traj_parallel, wall_parallel) * wall_parallel / jnp.linalg.norm(wall_parallel)**2
+    v_perp = jnp.dot(traj_parallel, wall_perp) * wall_perp / jnp.linalg.norm(wall_perp)**2
+
+    p = traj_start + t1*traj_parallel + (1-t1) * (-v_perp+v_parallel)
+
+    return lax.cond((t1 >= 0.0) & (t1 <= 1.0) & (t2 >= 0.0) & (t2 <= 1.0), lambda: (t1, p), lambda: (float("nan")*jnp.ones_like(t1), traj_end))
+
+
+@jit
+def calc_perpendicular_wall_distance(p, wall_start, wall_end):
+    """
+    Returns
+    ---------
+    d:
+        distance of p to wall
+    n:
+        normal of wall (oriented toward p)
+    """
+    wall_parallel = wall_end - wall_start
+    wall_perp = jnp.array([-wall_parallel[1], wall_parallel[0]])
+
+    v1 = p - wall_start
+    d = jnp.abs(jnp.cross(wall_parallel, v1) / jnp.linalg.norm(wall_parallel))
+    n_sign = jnp.sign(jnp.dot(wall_perp, v1))
+    n_sign = lax.cond(n_sign == 0, lambda: jnp.array(1.0), lambda: n_sign)
+    n = n_sign * wall_perp / jnp.linalg.norm(wall_perp)
+
+    return d, n
+
+@jit
+def calc_radial_wall_distance(p, wall_start, wall_end):
+    """
+    Returns
+    ---------
+    d:
+        distance of p to wall
+    n:
+        normal of wall (oriented toward p)
+    """
+    d_to_wall_extremities = jnp.array([((wall_start-p)**2).sum(), ((wall_end-p)**2).sum()])
+    closest_extremity_idx = jnp.argmin(d_to_wall_extremities)
+    d = jnp.sqrt(d_to_wall_extremities[closest_extremity_idx])
+    closest_extremity = lax.switch(closest_extremity_idx, [lambda: wall_start, lambda: wall_end])
+    other_extremity = lax.switch(closest_extremity_idx, [lambda: wall_end, lambda: wall_start])
+    n = lax.cond(d == 0, lambda: (p-other_extremity)/jnp.linalg.norm(p-other_extremity), lambda: (p - closest_extremity) / d)
+    return d, n
+
+@jit
+def wall_force_field_collision(traj_start, traj_end, wall_start, wall_end, sigma=0.5):
+    """
+    Returns the new trajectory endpoint if the trajectory is colliding with a wall that emits a repulsing force field.
+
+    Parameters
+    ---------
+    traj_start, traj_end, wall_start, wall_end: points (2D or 3D array)
+
+    Returns
+    ---------
+    d:
+        distance of traj_start to wall
+    p:
+        new traj_end point after collision
+    """
+    is_traj_start_above_wall_start = jnp.sign(jnp.dot(wall_end-wall_start, traj_start-wall_start)) >= 0
+    is_traj_start_below_wall_end = jnp.sign(jnp.dot(wall_start - wall_end, traj_start - wall_end)) >= 0
+
+    d_start, wall_normal = lax.cond(is_traj_start_above_wall_start & is_traj_start_below_wall_end,
+                                    calc_perpendicular_wall_distance, calc_radial_wall_distance,
+                                    traj_start, wall_start, wall_end)
+
+    traj_parallel = traj_end - traj_start
+    wall_parallel = jnp.array([-wall_normal[1], wall_normal[0]])
+
+    v_parallel = jnp.dot(traj_parallel, wall_parallel) * wall_parallel / jnp.linalg.norm(wall_parallel)**2
+    v_perp = jnp.dot(traj_parallel, wall_normal) * wall_normal
+
+    alpha = -flat_top_gaussian(d_start, x0=0, sigma=sigma, A=2, P=1) #alpha is between (-2, 0)
+    is_going_toward = -jnp.sign(jnp.dot(v_perp, wall_normal))
+    f_perp = is_going_toward*alpha*v_perp
+    dt = 1
+    v_perp = v_perp + f_perp*dt
+    v = v_perp+v_parallel
+    p = traj_start + v * dt
+
+    t1, p_prime = wall_elastic_collision(traj_start, p, wall_start, wall_end)
+
+    return lax.cond(jnp.isnan(t1),
+                    lambda: (d_start, p),
+                    lambda: (d_start, p_prime))
---
+++ test/test_misc.py
@@ -0,0 +1,109 @@
+from autodiscjax.utils.misc import wall_sticky_collision, wall_elastic_collision, wall_force_field_collision
+import jax.numpy as jnp
+import jax.random as jrandom
+from jax import vmap
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+
+def test_wall_elastic_collision():
+    # no intersection
+    assert jnp.isnan(
+        wall_elastic_collision(jnp.array([0., 0.]), jnp.array([1., 1.]), jnp.array([2., 2.]), jnp.array([3., 3.]))[0]).all()
+    assert jnp.isnan(
+        wall_elastic_collision(jnp.array([0., 0.]), jnp.array([1., 1.]), jnp.array([1.1, 1.1]), jnp.array([0.1, 0.1]))[0]).all()
+
+    # colinear
+    assert jnp.isnan(
+        wall_elastic_collision(jnp.array([0., 0.]), jnp.array([1., 1.]), jnp.array([0., 0.]), jnp.array([1., 1.]))[0]).all()
+    assert jnp.isnan(
+        wall_elastic_collision(jnp.array([0., 0.]), jnp.array([1., 1.]), jnp.array([1., 1.]), jnp.array([2., 2.]))[0]).all()
+
+
+
+    # intersection
+    assert wall_elastic_collision(jnp.array([0., 0.]), jnp.array([1., 1.]), jnp.array([1., 1.]),
+                                 jnp.array([0., 2.]))[0] == 1.0
+    assert (wall_elastic_collision(jnp.array([0., 0.]), jnp.array([1., 1.]), jnp.array([1., 1.]),
+                                 jnp.array([0., 2.]))[1] == jnp.array([1., 1.])).all()
+    assert wall_elastic_collision(jnp.array([0., 0.]), jnp.array([1., 1.]), jnp.array([0., 1.]),
+                                jnp.array([1., 0.]))[0] == 0.5
+    assert (((wall_elastic_collision(jnp.array([0., 0.]), jnp.array([1., 1.]), jnp.array([0., 1.]),
+                                 jnp.array([1., 0.]))[1] - jnp.array([0., 0.]))**2).sum() < 1e-3).all()
+    assert (((wall_elastic_collision(jnp.array([0., 0.]), jnp.array([2., 1.]), jnp.array([1., 0.]),
+                             jnp.array([1., 1.]))[1] - jnp.array([0., 1.])) ** 2).sum() < 1e-3).all()
+
+    key = jrandom.PRNGKey(0)
+    for _ in range(10):
+        key, subkey = jrandom.split(key)
+        A, B, C, D = jrandom.uniform(subkey, shape=(4,2))
+        assert (((wall_elastic_collision(A, B, C, D)[1] - wall_elastic_collision(A, B, D, C)[1]) ** 2).sum() < 1e-3).all()
+        assert (jnp.dot((wall_elastic_collision(A, B, C, D)[1] - wall_elastic_collision(B, A, C, D)[1]), (D-C)) -
+                 jnp.dot(B-A, D-C)) < 1e-3
+
+
+def test_wall_force_field_collision():
+    key = jrandom.PRNGKey(0)
+    A, B, C, D = jnp.array([0., 0.]), jnp.array([1., 1.]), jnp.array([0., 0.]), jnp.array([1., 1.])
+    for _ in range(100):
+        key, subkey = jrandom.split(key)
+        sigma = 5*jrandom.uniform(subkey)
+        d, p = wall_force_field_collision(A, B, C, D, sigma=sigma)
+        d2, p2 = wall_force_field_collision(A, B, D, C, sigma=sigma)
+        assert (d - d2) < 1e-3 and ((p - p2) ** 2).sum() < 1e-3
+        assert (d-0) < 1e-3 and ((p-jnp.array([1., 1.]))**2).sum() < 1e-3
+
+        d, p = wall_force_field_collision(jnp.array([0.5, 0.5]), jnp.array([1.5, 0.5]), jnp.array([1., 0.]), jnp.array([1., 1.]), sigma=sigma)
+        assert (d-0.5) < 1e-3 and (p[1]-0.5) < 1e-3 and (-0.5 < p[0]) and (p[0] < 1.0)
+
+
+def test_visualize_wall_collision(variant="force_field"):
+    key = jrandom.PRNGKey(0)
+
+    dt = 0.2
+    n = 50
+    L = 10
+    sigma = L / 10
+    w = 1 / 3
+    n_steps = 800
+
+    key, subkey = jrandom.split(key)
+    particles_pos = jrandom.uniform(subkey, minval=0, maxval=L, shape=(n, 2))
+    particles_f = jrandom.uniform(subkey, minval=-2, maxval=2., shape=(n, 2))
+    particles_v = jnp.zeros((n, 2)) + particles_f * dt
+
+    key, subkey = jrandom.split(key)
+    wall_start = L * jrandom.uniform(subkey, shape=(2,))
+    key, subkey = jrandom.split(key)
+    wall_end = wall_start + 2 * w * L * jrandom.uniform(subkey, shape=(2,)) - w * L
+
+    fig = plt.figure(figsize=(7, 7))
+    ax = plt.axes(xlim=(0, L), ylim=(0, L))
+    scatter = ax.scatter(particles_pos[:, 0], particles_pos[:, 1])
+    ax.plot([wall_start[0], wall_end[0]], [wall_start[1], wall_end[1]])
+    plt.axis("off")
+
+    all_positions = [particles_pos]
+    for step_idx in range(n_steps):
+        if variant == "force_field":
+            _, particles_pos = vmap(wall_force_field_collision, in_axes=(0, 0, None, None, None))(particles_pos,
+                                                                                                  particles_pos + particles_v * dt,
+                                                                                                  wall_start, wall_end,
+                                                                                                  sigma)
+        elif variant == "elastic":
+            _ , particles_pos = vmap(wall_elastic_collision, in_axes=(0, 0, None, None))(particles_pos, particles_pos + particles_v * dt, wall_start, wall_end)
+
+        elif variant == "sticky":
+            _, particles_pos = vmap(wall_sticky_collision, in_axes=(0, 0, None, None))(particles_pos, particles_pos + particles_v * dt, wall_start, wall_end)
+
+        particles_pos = particles_pos % L
+        all_positions.append(particles_pos)
+
+    def update(i):
+        scatter.set_offsets(all_positions[i])
+        return scatter,
+
+    anim = FuncAnimation(fig, update, frames=n_steps + 1, interval=50)
+    plt.show()
+
+
